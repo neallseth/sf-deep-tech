559884b75b5e474f3c0e799c26a423f55ad7bd50
neallseth <32910906+neallseth@users.noreply.github.com>
2024-03-02 15:56:12 -0800

    adding h-dvh to background canvas

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 
 export default function Home() {
   return (
-    <WavyBackground className="max-w-screen">
+    <WavyBackground className="max-w-screen h-dvh">
       <main className="flex h-dvh flex-col items-center justify-around p-4 sm:p-12 md:p-24 gap-24">
         <h1 className="text-white text-3xl sm:text-4xl md:text-6xl font-semibold whitespace-nowrap">
           SF Deep Tech Week
